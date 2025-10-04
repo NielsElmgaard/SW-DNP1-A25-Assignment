532ebf22976a3977b06bae3ba733d2d0b856eae7
niels <353468@viauc.dk>
2025-10-04 15:11:52 +0200

    part4 - 1st commit (step 4.1-4.3) + start of step 4.4

---
+++ Server/WebAPI/Controllers/CommentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RepositoryContracts;
+
+namespace WebAPI.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class CommentsController : ControllerBase
+{
+    private readonly ICommentRepository _commentRepository;
+
+    public CommentsController(ICommentRepository commentRepository)
+    {
+        _commentRepository = commentRepository;
+    }
+}
